963007131608569e4ff8aab76ac10294bd56d22c
loganlinn <logan@loganlinn.com>
2021-04-07 16:08:11 -0700

    implement plugin

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,51 +1,51 @@
 #!/usr/bin/env bash
+if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
+  # Bash 4.4, Zsh
+  set -euo pipefail
+else
+  # Bash 4.3 and older chokes on empty arrays with set -u.
+  set -eo pipefail
+fi
+shopt -s nullglob globstar
 
-set -euo pipefail
+require() { hash "$@" || exit 127; }
+require curl
+require git
+require sed
+require awk
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for terraformer.
-GH_REPO="https://github.com/GoogleCloudPlatform/terraformer"
-TOOL_NAME="terraformer"
-TOOL_TEST="terraformer --version"
+TOOL_NAME=terraformer
+GH_REPO=https://github.com/GoogleCloudPlatform/terraformer
 
 fail() {
-  echo -e "asdf-$TOOL_NAME: $*"
+  printf >&2 'asdf-%s: %s' "$TOOL_NAME" "$@"
   exit 1
 }
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if terraformer is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+  curl_opts+=(-H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
 sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
-list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' | LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if terraformer has other means of determining installable versions.
-  list_github_tags
+  git ls-remote --tags --refs "$GH_REPO" | grep -o 'refs/tags/.*' | cut -d/ -f3- | sed 's/^v//'
 }
 
 download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
+  local version="$1"
+  local download_path="$2"
 
-  # TODO: Adapt the release URL convention for terraformer
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  # i.e. terraformer-all-darwin-amd64
+  # i.e. terraformer-all-linux-amd64
+  url="$GH_REPO/releases/download/$version/terraformer-all-$(uname -s | tr '[:upper:]' '[:lower:]')-amd64"
 
   echo "* Downloading $TOOL_NAME release $version..."
-  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+  curl "${curl_opts[@]}" -o "$download_path" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
@@ -53,23 +53,15 @@ install_version() {
   local version="$2"
   local install_path="$3"
 
-  if [ "$install_type" != "version" ]; then
+  if [[ $install_type != version ]]; then
     fail "asdf-$TOOL_NAME supports release installs only"
   fi
 
-  # TODO: Adapt this to proper extension and adapt extracting strategy.
-  local release_file="$install_path/$TOOL_NAME-$version.tar.gz"
+  local release_file="$install_path/$TOOL_NAME"
   (
     mkdir -p "$install_path"
     download_release "$version" "$release_file"
-    tar -xzf "$release_file" -C "$install_path" --strip-components=1 || fail "Could not extract $release_file"
-    rm "$release_file"
-
-    # TODO: Asert terraformer executable exists.
-    local tool_cmd
-    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-    test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
-
+    chmod a+x "$release_file"
     echo "$TOOL_NAME $version installation was successful!"
   ) || (
     rm -rf "$install_path"
