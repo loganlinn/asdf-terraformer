97e043867f3b5ef1b456c82b9fd81c4c2d9c36e0
loganlinn <logan@loganlinn.com>
2021-04-07 16:50:06 -0700

    remove shopts

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
-  # Bash 4.4, Zsh
-  set -euo pipefail
-else
-  # Bash 4.3 and older chokes on empty arrays with set -u.
-  set -eo pipefail
-fi
-shopt -s nullglob globstar
+set -eo pipefail
 
 require() { hash "$@" || exit 127; }
 
